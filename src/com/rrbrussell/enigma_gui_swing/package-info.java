01d986a9e622e51017d05e22a8b71e0e8ba9c184
Robert R. Russell <robert@rrbrussell.com>
2014-05-02 12:56:26 -0500

    Start of package documentation

---
+++ src/com/rrbrussell/enigma_gui_swing/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (c) 2014 Robert R. Russell
+ */
+/**
+ * @author Robert R. Russell
+ * @author robert@rrbrussell.com
+ *
+ * Swing GUI for the German Enigma.
+ */
+package com.rrbrussell.enigma_gui_swing;
