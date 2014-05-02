bfb76669c2e8be3a0787d675468324533cb8bf6a
Robert R. Russell <robert@rrbrussell.com>
2014-05-02 13:09:37 -0500

    Start of the launcher

---
+++ src/com/rrbrussell/enigma_gui_swing/Main.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2014 Robert R. Russell
+ */
+package com.rrbrussell.enigma_gui_swing;
+
+/**
+ * @author Robert R. Russell
+ * @author robert@rrbrussell.com
+ * 
+ * The launcher for Swinging Enigma.
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
